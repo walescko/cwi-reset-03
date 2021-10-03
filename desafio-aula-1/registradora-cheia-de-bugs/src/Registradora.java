c68ec309cec0002ee9750d24e2de854445865e86
walescko <walescko@gmail.com>
2021-10-03 20:16:53 -0300

    Adicionando horário na classe Registradora

--- desafio-aula-1/registradora-cheia-de-bugs/src/Registradora.java
+++ desafio-aula-1/registradora-cheia-de-bugs/src/Registradora.java
@@ -43,42 +43,47 @@ public class Registradora {
 
         double precoTotal = registrarItem(item, quantidade);
 
-        System.out.println(String.format("Valor total: %.2f", precoTotal));
+        System.out.println(String.format("Valor total: R$ %.2f", precoTotal));
         System.out.println("----");
     }
 
     private static void segundoBug() {
         DataProjeto.criarDataComCozinhaEncerradaMasComDiaUtil();
+        System.out.println("Dia util? " +DataProjeto.getDiaUtil() + ", horário: "+ DataProjeto.getHora()+ ":"+ DataProjeto.getMinuto()); //apresetando data
+
         String item = "torta";
         int quantidade = 10;
 
         double precoTotal = registrarItem(item, quantidade);
 
-        System.out.println(String.format("Valor total: %.2f", precoTotal));
+        System.out.println(String.format("Valor total: R$ %.2f", precoTotal));
         System.out.println("----");
     }
 
     private static void terceiroBug() {
         DataProjeto.criarDataComCozinhaFuncionando();
+        System.out.println("Dia util? " +DataProjeto.getDiaUtil() + ", horário: "+ DataProjeto.getHora()+ ":"+ DataProjeto.getMinuto()); //apresetando data
+
         String item = "cafe";
         int quantidade = 40;
 
         double precoTotal = registrarItem(item, quantidade);
 
-        System.out.println(String.format("Valor total: %.2f", precoTotal));
+        System.out.println(String.format("Valor total: R$ %.2f", precoTotal));
         System.out.println("----");
     }
 
     private static void quartoBug() {
         DataProjeto.criarDataComCozinhaFuncionando();
+        System.out.println("Dia util? " +DataProjeto.getDiaUtil() + ", horário: "+ DataProjeto.getHora()+ ":"+ DataProjeto.getMinuto()); //apresetando data
+
         // Cliente 1
         String item = "sanduiche";
         int quantidade = 20;
 
         double precoTotal = registrarItem(item, quantidade);
 
-        System.out.println(String.format("Valor total: %.2f", precoTotal));
-        System.out.println("----");
+        System.out.println(String.format("Valor total: R$ %.2f", precoTotal));
 
         // Cliente 2
         String item2 = "sanduiche";
@@ -86,30 +91,34 @@ public class Registradora {
 
         double precoTotal2 = registrarItem(item2, quantidade2);
 
-        System.out.println(String.format("Valor total: %.2f", precoTotal2));
+        System.out.println(String.format("Valor total: R$ %.2f", precoTotal2));
         System.out.println("----");
     }
 
     private static void quintoBug() {
         DataProjeto.criarDataComCozinhaFuncionando();
+        System.out.println("Dia util? " +DataProjeto.getDiaUtil() + ", horário: "+ DataProjeto.getHora()+ ":"+ DataProjeto.getMinuto()); //apresetando data
+
         String item = "pao";
         int quantidade = 10;
 
         double precoTotal = registrarItem(item, quantidade);
 
-        System.out.println(String.format("Valor total: %.2f", precoTotal));
+        System.out.println(String.format("Valor total: R$ %.2f", precoTotal));
         System.out.println("----");
     }
 
     private static void sextoBug() {
         DataProjeto.criarDataComCozinhaEncerradaSemDiaUtil();
+        System.out.println("Dia util? " +DataProjeto.getDiaUtil() + ", horário: "+ DataProjeto.getHora()+ ":"+ DataProjeto.getMinuto()); //apresetando data
+
         // Cliente 1
         String item = "sanduiche";
         int quantidade = 20;
 
         double precoTotal = registrarItem(item, quantidade);
 
-        System.out.println(String.format("Valor total: %.2f", precoTotal));
+        System.out.println(String.format("Valor total: R$ %.2f", precoTotal));
 
         // Cliente 2
         String item2 = "sanduiche";
@@ -117,7 +126,7 @@ public class Registradora {
 
         double precoTotal2 = registrarItem(item2, quantidade2);
 
-        System.out.println(String.format("Valor total: %.2f", precoTotal2));
+        System.out.println(String.format("Valor total: R$ %.2f", precoTotal2));
         System.out.println("----");
     }
 
