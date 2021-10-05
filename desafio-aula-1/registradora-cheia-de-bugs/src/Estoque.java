c4390f161d543b84454b27eda35eacb7a451a948
walescko <walescko@gmail.com>
2021-10-05 20:33:17 -0300

    Implementando a classe controle de estoque

---
+++ desafio-aula-1/registradora-cheia-de-bugs/src/Estoque.java
@@ -0,0 +1,9 @@
+public class Estoque {
+
+    protected int paoEstoque;
+    protected int tortaEstoque;
+    protected int cafeEstoque;
+    protected int sanduicheEstoque;
+    protected int leiteEstoque;
+
+}
