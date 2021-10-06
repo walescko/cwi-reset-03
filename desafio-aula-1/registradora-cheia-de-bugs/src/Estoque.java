e48f3b633083cf27eca8f786b2986e9573cd13cc
walescko <walescko@gmail.com>
2021-10-05 23:10:01 -0300

    Classe não necessária

--- desafio-aula-1/registradora-cheia-de-bugs/src/Estoque.java
+++
@@ -1,9 +0,0 @@
-public class Estoque {
-
-    protected int paoEstoque;
-    protected int tortaEstoque;
-    protected int cafeEstoque;
-    protected int sanduicheEstoque;
-    protected int leiteEstoque;
-
-}
