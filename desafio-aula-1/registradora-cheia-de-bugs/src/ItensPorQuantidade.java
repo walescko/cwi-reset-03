4741ee2b1fa2322db382bbc2929994b7ace38ed9
walescko <walescko@gmail.com>
2021-10-05 22:54:26 -0300

    Adicionando controle de estoque e baixa estoque - apenas para o Sanduiche

--- desafio-aula-1/registradora-cheia-de-bugs/src/ItensPorQuantidade.java
+++ desafio-aula-1/registradora-cheia-de-bugs/src/ItensPorQuantidade.java
@@ -1,10 +1,23 @@
 public class ItensPorQuantidade {
 
-    static int pao = 3600;
-    static int torta = 4;
-    static int sanduiche = 20;
-    static int leite = 20;
-    static int cafe = 20;
+    static int pao = 3600; //peso
+    static int torta = 4; //unidade de torta, cliente pede por fatia, então temos aqui 4 tortas totalizando 64 fatias
+    static int sanduiche = 20; //unidade
+    static int leite = 20; //unidade
+    static int cafe = 20; //unidade
 
+    //Estoque de Sanduiche
+    static void Estoque() {
+        System.out.println("Sanduiches: " + sanduiche);
+    }
 
+    static void baixaEstoque(String item, int qtd) {
+       if ("sanduiche".equals(item)){
+           if (sanduiche >= qtd){
+           sanduiche -= qtd;
+            } else {
+               System.out.println("Sem Estoque para realizar o pedido!");
+           }
+       }
+    }
 }
