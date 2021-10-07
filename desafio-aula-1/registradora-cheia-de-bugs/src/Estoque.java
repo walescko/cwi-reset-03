d218969c8ab32248d003f0ac2c6d0aa659cf08fe
walescko <walescko@gmail.com>
2021-10-06 22:34:26 -0300

    adição do getters e setters

--- desafio-aula-1/registradora-cheia-de-bugs/src/Estoque.java
+++ desafio-aula-1/registradora-cheia-de-bugs/src/Estoque.java
@@ -5,19 +5,101 @@ public class Estoque {
     static int sanduiche = 20; //unidade
     static int leite = 20; //unidade
     static int cafe = 20; //unidade
+    static int fatia = torta*16; //variavel auxiliar para o controle das tortas
 
-    //Estoque de Sanduiche
+    //Estoque
+
     static void EstoquePadaria() {
+        System.out.println("\n------- Estoque -------");
         System.out.println("Sanduiches: " + sanduiche);
+        System.out.println("Leite: " + leite);
+        System.out.println("Cafe: "+ cafe);
+        System.out.println("Fatias de Torta: " + fatia);
+        System.out.println("Pão (gramas): " + pao);
+        System.out.println("------   ------\n");
     }
 
     static void baixaEstoque(String item, int qtd) {
-       if ("sanduiche".equals(item)){
-           if (sanduiche >= qtd){
-           sanduiche -= qtd;
+       if ("pao".equals(item)){
+           int peso = qtd*60;
+           if (pao >= peso){
+           pao -= peso;
             } else {
-               System.out.println("Sem Estoque para realizar o pedido!");
+               ReposicaoCozinha.reporItem(item);
+           }
+       } else if ("torta".equals(item)){
+           if (fatia >= qtd){
+               fatia -= qtd;
+           } else {
+               ReposicaoCozinha.reporItem(item);
+           }
+       } else if ("sanduiche".equals(item)){
+           if (sanduiche >= qtd){
+               sanduiche -= qtd;
+           } else {
+               ReposicaoCozinha.reporItem(item);
+           }
+       } else if ("leite".equals(item)){
+           if (leite >= qtd){
+               leite -= qtd;
+           } else{
+               ReposicaoFornecedor.reporItem(item);
+           }
+        } else if ("cafe".equals(item)){
+           if (cafe >= qtd){
+               cafe -= qtd;
+           } else {
+               ReposicaoFornecedor.reporItem(item);
            }
        }
+
+    }
+
+     public static int getPao() {
+        return pao;
+    }
+
+    public static void setPao(int pao) {
+        Estoque.pao = pao;
+    }
+
+    public static int getTorta() {
+        return torta;
+    }
+
+    public static void setTorta(int torta) {
+        Estoque.torta = torta;
+    }
+
+    public static int getSanduiche() {
+        return sanduiche;
+    }
+
+    public static void setSanduiche(int sanduiche) {
+        Estoque.sanduiche = sanduiche;
+    }
+
+    public static int getLeite() {
+        return leite;
+    }
+
+    public static void setLeite(int leite) {
+        Estoque.leite = leite;
+    }
+
+    public static int getCafe() {
+        return cafe;
+    }
+
+    public static void setCafe(int cafe) {
+        Estoque.cafe = cafe;
+    }
+
+    public static int getFatia() {
+        return fatia;
+    }
+
+    public static void setFatia(int fatia) {
+        Estoque.fatia = fatia;
     }
 }
