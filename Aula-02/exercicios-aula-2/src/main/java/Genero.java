623895546769dd95947f03d7198b86096391d6bc
walescko <walescko@gmail.com>
2021-10-09 12:15:03 -0300

    fix Classe Genero criada

--- Aula-02/exercicios-aula-2/src/main/java/Genero.java
+++ Aula-02/exercicios-aula-2/src/main/java/Genero.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public enum Genero {
+public enum Genero {
+    MASCULINO("Masculino"),
+    FEMININO("Feminino"),
+    NAO_BINARIO("Não Binário");
+
+    private String genero;
+
+    Genero(String genero){
+        this.genero = genero;
+    }
+    public String getGenero(){
+        return this.genero;
+    }
+
+
 }
