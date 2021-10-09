8a5d28aa5116ae66ad135d2152a33f14f29c9e31
walescko <walescko@gmail.com>
2021-10-09 15:14:45 -0300

    Criando novo ator e diretor com a nova classe Pessoa e usando herança entre as classes

--- Aula-02/exercicios-aula-2/src/main/java/Aplicacao.java
+++ Aula-02/exercicios-aula-2/src/main/java/Aplicacao.java
@@ -2,19 +2,20 @@ public class Aplicacao {
 
     public static void main(String[] args){
 
-        DiretorFilme diretor01 = new DiretorFilme("Gaspar Lucas", 44, 5,
-                Genero.FEMININO);
-        Ator ator01 = new Ator("Pierce Browson", 55, 25, Genero.MASCULINO);
-        Filme filme01 = new Filme("A volta dos que nao foram", "Aventura", 144,
-                1985, 5, diretor01);
+        Pessoa diretor01 = new DiretorFilme("Gaspar Lucas Spilberg", 44, Genero.FEMININO, 5);
 
-        Filme filme02 = new Filme("Poeira em alto mar", "Comédia", 122,
-                1990, 4, diretor01);
+        Pessoa ator01 = new Ator("Pierce Browson", 55, Genero.MASCULINO, 3);
+
+//        Filme filme01 = new Filme("A volta dos que nao foram", "Aventura", 144,
+//                1985, 5, diretor01);
+//
+//        Filme filme02 = new Filme("Poeira em alto mar", "Comédia", 122,
+//                1990, 4, diretor01);
 
 //        filme01.apresentarFilme();
 //        filme02.apresentarFilme();
-        diretor01.diretorApresentacao();
-        ator01.apresentacaoAtor();
+        diretor01.Apresentacao();
+        ator01.Apresentacao();
 
 
 
