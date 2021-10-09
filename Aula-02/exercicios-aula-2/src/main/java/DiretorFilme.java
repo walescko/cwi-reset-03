a3afd3fe6ea6b9183709a4b1b3dbf5291a64c69a
walescko <walescko@gmail.com>
2021-10-09 12:15:41 -0300

    Genero adiconado na classe diretor

--- Aula-02/exercicios-aula-2/src/main/java/DiretorFilme.java
+++ Aula-02/exercicios-aula-2/src/main/java/DiretorFilme.java
@@ -1,20 +1,23 @@
 public class DiretorFilme {
 
-    private static String nomeDiretor;
+    private  static String nomeDiretor;
     private static int idadeDiretor;
     private static int filmesDirigidos;
+    private static Genero genero;
 
-    public DiretorFilme(String nomeDiretor, int idadeDiretor, int filmesDirigidos) {
+    public DiretorFilme(String nomeDiretor, int idadeDiretor, int filmesDirigidos, Genero genero) {
         this.nomeDiretor = nomeDiretor;
         this.idadeDiretor = idadeDiretor;
         this.filmesDirigidos = filmesDirigidos;
+        this.genero = genero;
     }
 
-    public static void diretorEstatistica() {
+    public static void diretorApresentacao() {
         System.out.println(" ---- Diretor ---- ");
         System.out.println("Nome: " + getNomeDiretor());
         System.out.println("Idade: " + getIdadeDiretor());
         System.out.println("Filmes dirigidos: " + getFilmesDirigidos());
+        System.out.println("Genero: " + getGenero());
         System.out.println(" ----------------- ");
         }
 
@@ -28,5 +31,9 @@ public class DiretorFilme {
 
     public static int getFilmesDirigidos() {
         return filmesDirigidos;
+    }
+
+    public static Genero getGenero() {
+        return genero;
     }
 }
