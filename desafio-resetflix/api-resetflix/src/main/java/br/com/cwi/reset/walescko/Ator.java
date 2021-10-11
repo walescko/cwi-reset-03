d7cfb2d4a3459ea0fa75956fbbb6fd80dc514953
walescko <walescko@gmail.com>
2021-10-11 19:57:48 -0300

    Automação do ID e criação de getNome

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/walescko/Ator.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/walescko/Ator.java
@@ -3,19 +3,22 @@ package br.com.cwi.reset.walescko;
 import jdk.jshell.Snippet;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public class Ator extends Pessoa {
 
-    private Integer id;
+    private String id;
     private StatusCarreira statusCarreira;
+    private String nome;
 
-    public Ator(String nome, LocalDate dataNascimento, LocalDate inicioAtividade, StatusCarreira statusCarreira, Integer id){
+    public Ator(String nome, LocalDate dataNascimento, LocalDate inicioAtividade, StatusCarreira statusCarreira, String id){
         super(nome, dataNascimento, inicioAtividade);
         this.statusCarreira = statusCarreira;
-        this.id = id;
+        id = UUID.randomUUID().toString();
+        this.id = id ;
     }
 
-
-
-
+    public String getNome() {
+        return nome;
+    }
 }
