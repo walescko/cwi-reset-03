180eec01a4af4db960b51a74f258e900f68a5b5c
walescko <walescko@gmail.com>
2021-10-11 19:16:57 -0300

    Criando o Construtor Ator e Diretor em suas respectivas classes

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/walescko/Ator.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/walescko/Ator.java
@@ -2,10 +2,18 @@ package br.com.cwi.reset.walescko;
 
 import jdk.jshell.Snippet;
 
-public class Ator {
+import java.time.LocalDate;
+
+public class Ator extends Pessoa {
 
     private Integer id;
     private StatusCarreira statusCarreira;
+
+    public Ator(String nome, LocalDate dataNascimento, LocalDate inicioAtividade, StatusCarreira statusCarreira, Integer id){
+        super(nome, dataNascimento, inicioAtividade);
+        this.statusCarreira = statusCarreira;
+        this.id = id;
+    }
 
 
 
--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/walescko/Diretor.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/walescko/Diretor.java
@@ -1,8 +1,13 @@
 package br.com.cwi.reset.walescko;
 
+import java.time.LocalDate;
+
 public class Diretor {
 
     private Integer id;
 
-
+    public Diretor(String nome, LocalDate dataNascimento, LocalDate anoInicioAtividade, Integer id) {
+        super(nome, dataNascimento, anoInicioAtividade);
+        this.id = id;
+    }
 }
