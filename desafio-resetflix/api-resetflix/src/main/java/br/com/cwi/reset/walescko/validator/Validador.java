21282d85292d8bfa7afaa62ae70a2193bc6688e1
walescko <walescko@gmail.com>
2021-10-17 19:48:14 -0300

    Ajuste da classe de validação - correção do nome do método

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/walescko/validator/Validador.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/walescko/validator/Validador.java
@@ -1,2 +1,35 @@
-package br.com.cwi.reset.walescko.validator;public class Validador {
+package br.com.cwi.reset.walescko.validator;
+
+import br.com.cwi.reset.walescko.mensagemExceptions.*;
+
+import java.time.LocalDate;
+
+public class Validador {
+
+
+    public void accept(final String nome, final LocalDate dataNascimento, final Integer anoInicioAtividade, final TipoDominioException tipoDominioException) throws Exception {
+        if (nome == null) {
+            throw new NomeNaoInformadoException();
+        }
+
+        if (dataNascimento == null) {
+            throw new DataNascimentoNaoInformadoException();
+        }
+
+        if (anoInicioAtividade == null) {
+            throw new AnoInicioAtividadeNaoInformadoException();
+        }
+
+        if (nome.split(" ").length < 2) {
+            throw new NomeSobrenomeObrigatorioException(tipoDominioException.getSingular());
+        }
+
+        if (LocalDate.now().isBefore(dataNascimento)) {
+            throw new NascidosNoFuturoException(tipoDominioException.getPlural());
+        }
+
+        if (anoInicioAtividade <= dataNascimento.getYear()) {
+            throw new AnoInicioAtividadeInvalidoException(tipoDominioException.getSingular());
+        }
+    }
 }
