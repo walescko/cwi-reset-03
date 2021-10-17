821b6e9db23bf9a39ce0a400502cefdb143976ff
walescko <walescko@gmail.com>
2021-10-17 10:38:39 -0300

    Classe main da aula 3

--- Aula-03/primeiro-projeto-spring/src/main/java/br/com/cwi/reset/primeiroprojetospring/PrimeiroProjetoSpringApplication.java
+++ Aula-03/primeiro-projeto-spring/src/main/java/br/com/cwi/reset/primeiroprojetospring/PrimeiroProjetoSpringApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PrimeiroProjetoSpringApplication {
 
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(PrimeiroProjetoSpringApplication.class, args);
+
 	}
 
 }
