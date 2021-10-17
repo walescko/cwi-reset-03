d791fb158b17b14dbe0d19c0c724c8edd2f85b77
walescko <walescko@gmail.com>
2021-10-17 19:47:29 -0300

    Ajustando consulda Id Invalida

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/walescko/mensagemExceptions/ConsultaIdInvalidoException.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/walescko/mensagemExceptions/ConsultaIdInvalidoException.java
@@ -1,7 +1,8 @@
 package br.com.cwi.reset.walescko.mensagemExceptions;
 
-public class ConsultaIdInvalidoException extends Exception{
+public class ConsultaIdInvalidoException extends Exception {
 
     public ConsultaIdInvalidoException(String tipo, Integer id) {
         super(String.format("Nenhum %s encontrado com o parâmetro id = %d, favor verificar os parâmetros informados.", tipo, id));
+    }
 }
