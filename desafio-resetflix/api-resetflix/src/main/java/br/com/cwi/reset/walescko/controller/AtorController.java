30ec7561527490284b1a3ea61b89427a9ca8ade1
walescko <walescko@gmail.com>
2021-10-20 22:57:33 -0300

    Métodos de criar e listar atores

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/walescko/controller/AtorController.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/walescko/controller/AtorController.java
@@ -1,4 +1,43 @@
 package br.com.cwi.reset.walescko.controller;
 
+import br.com.cwi.reset.walescko.FakeDatabase;
+import br.com.cwi.reset.walescko.models.Ator;
+import br.com.cwi.reset.walescko.request.AtorRequest;
+import br.com.cwi.reset.walescko.response.AtorEmAtividade;
+import br.com.cwi.reset.walescko.service.AtorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/atores")
 public class AtorController {
+
+    private AtorService atorService;
+    private FakeDatabase fakeDatabase;
+
+    private static List<Ator> atores = new ArrayList<>();
+
+
+    public AtorController() {
+        this.atorService = new AtorService(FakeDatabase.getInstance());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void criarAtor(@RequestBody AtorRequest atorRequest) throws Exception {
+        this.atorService.criarAtor(atorRequest);
+    }
+
+    @GetMapping
+    public List<Ator> consultarAtores() throws Exception{
+        return this.atorService.consultarAtores();
+    }
+
+    @GetMapping(path = "/em_atividade")
+    public List<AtorEmAtividade> listarAtoresEmAtividade(@RequestParam String filtroNome) throws Exception {
+        return this.atorService.listarAtoresEmAtividade(filtroNome);
+    }
 }
