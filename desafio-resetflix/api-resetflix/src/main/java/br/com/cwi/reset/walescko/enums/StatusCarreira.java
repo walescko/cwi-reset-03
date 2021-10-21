5609581aab5e534f8eba1adacb3c8fbb083eacc2
walescko <walescko@gmail.com>
2021-10-20 22:56:48 -0300

    Arrumando as mensagens do Status de Carreira

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/walescko/enums/StatusCarreira.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/walescko/enums/StatusCarreira.java
@@ -1,8 +1,16 @@
 package br.com.cwi.reset.walescko.enums;
 
 public enum StatusCarreira {
-    EM_ATIVIDADE,
-    APOSENTADO;
+    EM_ATIVIDADE("Em atividade"),
+    APOSENTADO("Aposentado");
 
+    private String statusCarreira;
 
+    StatusCarreira(String statusCarreira) {
+        this.statusCarreira = statusCarreira;
+    }
+
+    public String getStatusCarreira(){
+        return statusCarreira;
+    }
 }
